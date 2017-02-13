88aa7fd1d43ffd7140d4087a7301d88c78024b1b
Paul Currit <pcurrit@hotmail.com>
2017-02-13 13:51:29 -0800

    initial map component using leaflet and prop types taken from react-map-gl

---
+++ src/react.map.js
@@ -0,0 +1,186 @@
+import React, {Component, PropTypes} from 'react';
+import config from './react-map-gl/src/config';
+import Leaflet from 'leaflet';
+import Immutable from 'immutable';
+
+// purloined from react-map-gl
+const PROP_TYPES = {
+  /**
+    * The latitude of the center of the map.
+    */
+  latitude: PropTypes.number.isRequired,
+  /**
+    * The longitude of the center of the map.
+    */
+  longitude: PropTypes.number.isRequired,
+  /**
+    * The tile zoom level of the map.
+    */
+  zoom: PropTypes.number.isRequired,
+  /**
+    * The maximum tile zoom level of the map. Defaults to 20.
+    * Increasing this will allow you to zoom further into the map but should
+    * only be used if you know what you are doing past zoom 20. The default
+    * map styles won't render anything useful past 20.
+    */
+  maxZoom: PropTypes.number,
+  /**
+    * The Mapbox style the component should use. Can either be a string url
+    * or a MapboxGL style Immutable.Map object.
+    */
+  mapStyle: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.instanceOf(Immutable.Map)
+  ]),
+  /**
+    * The Mapbox API access token to provide to mapbox-gl-js. This is required
+    * when using Mapbox provided vector tiles and styles.
+    */
+  mapboxApiAccessToken: PropTypes.string,
+  /**
+    * `onChangeViewport` callback is fired when the user interacted with the
+    * map. The object passed to the callback contains `latitude`,
+    * `longitude` and `zoom` and additional state information.
+    */
+  onChangeViewport: PropTypes.func,
+  /**
+    * The width of the map.
+    */
+  width: PropTypes.number.isRequired,
+  /**
+    * The height of the map.
+    */
+  height: PropTypes.number.isRequired,
+  /**
+    * Is the component currently being dragged. This is used to show/hide the
+    * drag cursor. Also used as an optimization in some overlays by preventing
+    * rendering while dragging.
+    */
+  isDragging: PropTypes.bool,
+  /**
+    * Required to calculate the mouse projection after the first click event
+    * during dragging. Where the map is depends on where you first clicked on
+    * the map.
+    */
+  startDragLngLat: PropTypes.array,
+  /**
+    * Called when a feature is hovered over. Uses Mapbox's
+    * queryRenderedFeatures API to find features under the pointer:
+    * https://www.mapbox.com/mapbox-gl-js/api/#Map#queryRenderedFeatures
+    * To query only some of the layers, set the `interactive` property in the
+    * layer style to `true`. See Mapbox's style spec
+    * https://www.mapbox.com/mapbox-gl-style-spec/#layer-interactive
+    * If no interactive layers are found (e.g. using Mapbox's default styles),
+    * will fall back to query all layers.
+    * @callback
+    * @param {array} features - The array of features the mouse is over.
+    */
+  onHoverFeatures: PropTypes.func,
+  /**
+    * Defaults to TRUE
+    * Set to false to enable onHoverFeatures to be called regardless if
+    * there is an actual feature at x, y. This is useful to emulate
+    * "mouse-out" behaviors on features.
+    */
+  ignoreEmptyFeatures: PropTypes.bool,
+
+  /**
+    * Show attribution control or not.
+    */
+  attributionControl: PropTypes.bool,
+
+  /**
+   * Called when the map is clicked. The handler is called with the clicked
+   * coordinates (https://www.mapbox.com/mapbox-gl-js/api/#LngLat) and the
+   * screen coordinates (https://www.mapbox.com/mapbox-gl-js/api/#PointLike).
+   */
+  onClick: PropTypes.func,
+
+  /**
+    * Called when a feature is clicked on. Uses Mapbox's
+    * queryRenderedFeatures API to find features under the pointer:
+    * https://www.mapbox.com/mapbox-gl-js/api/#Map#queryRenderedFeatures
+    * To query only some of the layers, set the `interactive` property in the
+    * layer style to `true`. See Mapbox's style spec
+    * https://www.mapbox.com/mapbox-gl-style-spec/#layer-interactive
+    * If no interactive layers are found (e.g. using Mapbox's default styles),
+    * will fall back to query all layers.
+    */
+  onClickFeatures: PropTypes.func,
+
+  /**
+    * Radius to detect features around a clicked point. Defaults to 15.
+    */
+  clickRadius: PropTypes.number,
+
+  /**
+    * Passed to Mapbox Map constructor which passes it to the canvas context.
+    * This is unseful when you want to export the canvas as a PNG.
+    */
+  preserveDrawingBuffer: PropTypes.bool,
+
+  /**
+    * There are still known issues with style diffing. As a temporary stopgap,
+    * add the option to prevent style diffing.
+    */
+  preventStyleDiffing: PropTypes.bool,
+
+  /**
+    * Enables perspective control event handling
+    */
+  perspectiveEnabled: PropTypes.bool,
+
+  /**
+    * Specify the bearing of the viewport
+    */
+  bearing: PropTypes.number,
+
+  /**
+    * Specify the pitch of the viewport
+    */
+  pitch: PropTypes.number,
+
+  /**
+    * Specify the altitude of the viewport camera
+    * Unit: map heights, default 1.5
+    * Non-public API, see https://github.com/mapbox/mapbox-gl-js/issues/1137
+    */
+  altitude: PropTypes.number,
+
+  /**
+    * The load callback is called when all dependencies have been loaded and
+    * the map is ready.
+    */
+  onLoad: PropTypes.func
+
+};
+
+const DEFAULT_PROPS = {
+  mapStyle: 'mapbox://styles/mapbox/light-v9',
+  onChangeViewport: null,
+  mapboxApiAccessToken: config.DEFAULTS.MAPBOX_API_ACCESS_TOKEN,
+  preserveDrawingBuffer: false,
+  attributionControl: true,
+  ignoreEmptyFeatures: true,
+  bearing: 0,
+  pitch: 0,
+  altitude: 1.5,
+  clickRadius: 15,
+  maxZoom: 20
+};
+
+class Map extends Component {
+  componentDidMount() {
+    this.leafletMap = Leaflet.map(this.leafletElement).setView([this.props.latitude, this.props.longitude], this.props.zoom);
+  }
+  render() {
+    const {style, width, height} = this.props;
+    
+    return (
+      <div style={{...style, height, width}} ref={(ref) => this.leafletElement = ref}></div>
+    );
+
+  }
+}
+
+export default Map;
